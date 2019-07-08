a95d0821f5fc70e3760344f6adf4fcc883eb5447
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2019-07-08 10:08:15 -0400

    Últimos cambios antes de afinar detalles (#9)
    Se termina de realizar el merge entre el código de Adriana y el de Daniela

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 /* Manejo del DOM */
 
-
 /*Varibles para obtener data de cada país, ya separada*/
 //console.log(WORLDBANK);
 const chileData = WORLDBANK.CHL;
@@ -55,137 +54,133 @@ for (let i = 0; i < selectedData.indicators.length; i++) {
 
 let selectIndicator;
 document.getElementById("indicatorsSelect").addEventListener("change", () => {
-
   selectIndicator = document.getElementById("indicatorsSelect").value;
   mostrarIndicador();
+  showDoughnutMaxValue();
+  showDoughnutAverageValue();
 });
 
 var graphPlaceholder = document
   .getElementById("indicatorsChart")
   .getContext("2d");
 
- showDoughnutMaxValue();
- showDoughnutAverageValue();
-});
-
-
 //Agregando dona con el valor maximo
 function showDoughnutMaxValue() {
-	let indicatorNumber = selectIndicator;
-	let maxValue = 0;
-	let minValue = 0;
-	for (let year in selectedData.indicators[indicatorNumber].data) {
-		if (selectedData.indicators[indicatorNumber].data[year] != "") {
-     if (selectedData.indicators[indicatorNumber].data[year] > maxValue) {
-     		// console.log(selectedData.indicators[indicatorNumber].data[year]+" es mayor "+maxValue);
-     	 maxValue = selectedData.indicators[indicatorNumber].data[year]
-     }
-   }
-	}
-	minValue = 100 - maxValue;
-	var chart = document.getElementById('doughnutMax').getContext('2d');
-	
-	var config = {
-		type: 'doughnut',
-		data: {
-			datasets: [{
-				data: [maxValue,minValue],
-				backgroundColor: ['#6b48ff','darkgray'],
-			}],
+  let indicatorNumber = selectIndicator;
+  let maxValue = 0;
+  let minValue = 0;
+  for (let year in selectedData.indicators[indicatorNumber].data) {
+    if (selectedData.indicators[indicatorNumber].data[year] != "") {
+      if (selectedData.indicators[indicatorNumber].data[year] > maxValue) {
+        // console.log(selectedData.indicators[indicatorNumber].data[year]+" es mayor "+maxValue);
+        maxValue = selectedData.indicators[indicatorNumber].data[year];
+      }
+    }
+  }
+  minValue = 100 - maxValue;
+  var chart = document.getElementById("doughnutMax").getContext("2d");
 
-		},
-		options: {
+  var config = {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [maxValue, minValue],
+          backgroundColor: ["#6b48ff", "darkgray"]
+        }
+      ]
+    },
+    options: {
       layout: {
-            padding: {
-                left: 640,
-                right: 0,
-                top: 0,
-                bottom: 0
-            }
-        },
+        padding: {
+          left: 640,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
       title: {
         display: true,
-        text: 'Valor Máximo',
-        position: 'bottom'
+        text: "Valor Máximo",
+        position: "bottom"
       },
-			cutoutPercentage: 80,
+      cutoutPercentage: 80,
       plugins: {
         doughnutlabel: {
           labels: [
             {
-              text: maxValue+"%",
+              text: maxValue + "%",
               font: {
-                size: '100'
+                size: "100"
               },
-              color: 'grey'
-            },
+              color: "grey"
+            }
           ]
         }
       }
-		}
-	};
-	var doughnutMax = new Chart(chart, config);
+    }
+  };
+  var doughnutMax = new Chart(chart, config);
 }
-
 
 // Agrenado dona con valor promedio
 function showDoughnutAverageValue() {
-	let indicatorNumber = selectIndicator;
-	let count = 0;
-	let average = 0;
-	let mount = 0;
-	let minValue = 0;
-	for (let year in selectedData.indicators[indicatorNumber].data) {
-		if (selectedData.indicators[indicatorNumber].data[year] != "") {
-     	mount = mount + selectedData.indicators[indicatorNumber].data[year];
-     	count = count + 1;
-   	}
-	}
-	average = mount / count;
-	console.log(average);
-	minValue = 100 - average;
+  let indicatorNumber = selectIndicator;
+  let count = 0;
+  let average = 0;
+  let mount = 0;
+  let minValue = 0;
+  for (let year in selectedData.indicators[indicatorNumber].data) {
+    if (selectedData.indicators[indicatorNumber].data[year] != "") {
+      mount = mount + selectedData.indicators[indicatorNumber].data[year];
+      count = count + 1;
+    }
+  }
+  average = mount / count;
+  console.log(average);
+  minValue = 100 - average;
 
-	var chart = document.getElementById('doughnutAverages').getContext('2d');
+  var chart = document.getElementById("doughnutAverages").getContext("2d");
 
-var config = {
-			type: 'doughnut',
-			data: {
-				datasets: [{
-					data: [average,minValue],
-					backgroundColor: ['#6b48ff','darkgray'],
-				}],
-
-			},
-			options: {
-        cutoutPercentage: 80,
+  var config = {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [average, minValue],
+          backgroundColor: ["#6b48ff", "darkgray"]
+        }
+      ]
+    },
+    options: {
+      cutoutPercentage: 80,
       layout: {
-            padding: {
-                left: 0,
-                right: 640,
-                top: 0,
-                bottom: 0
-            }
-        },
-        title: {
-          display: true,
-          text: 'Valor Promedio',
-          position: 'bottom'
-        },
-        plugins: {
-          doughnutlabel: {
-            labels: [
-              {
-                text: average+"%",
-                font: {
-                  size: '100'
-                },
-                color: 'grey'
+        padding: {
+          left: 0,
+          right: 640,
+          top: 0,
+          bottom: 0
+        }
+      },
+      title: {
+        display: true,
+        text: "Valor Promedio",
+        position: "bottom"
+      },
+      plugins: {
+        doughnutlabel: {
+          labels: [
+            {
+              text: average + "%",
+              font: {
+                size: "100"
               },
-            ]
-          }
+              color: "grey"
+            }
+          ]
         }
       }
-		};
-	var doughnutMax = new Chart(chart, config);
+    }
+  };
+  var doughnutMax = new Chart(chart, config);
 }
-
